c5adc782aa19aec4bdfcafd01fdb30bf8d84bece
Arthur Asasira <asasiraarthur@gmail.com>
2025-06-28 13:05:51 +0300

    jjj

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client'],
   // Force all pages to be dynamic
+  output: 'standalone',
+
 };
 
 export default nextConfig;
